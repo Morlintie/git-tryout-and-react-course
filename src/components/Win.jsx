0d4c510528b01ad5ce94c26409de27a1713f5cfd
Morlintie <ahmetyasintoraman@gmail.com>
2025-02-20 12:52:50 +0300

    feature2

--- src/components/Win.jsx
+++ src/components/Win.jsx
@@ -2,7 +2,7 @@ export function Win() {
   return (
     <>
       <section className="win-section">
-        <h3>You !</h3>
+        <h3>You got it bro you got it !</h3>
         <p>Well done! 🎉 </p>
       </section>
     </>
